d7f21674c7c670b70ca7368fcddf6cb9f0b14b18
星塵織夢 <118804535+h1s97x@users.noreply.github.com>
2026-05-16 16:25:21 +0800

    refactor: 移除废弃的 hexo-renderer-jade 和 hexo-renderer-marked (#76)
    * refactor: 移除废弃的 hexo-renderer-jade 和 hexo-renderer-marked
    
    问题: 项目同时安装了多个 Markdown 渲染器，存在冲突风险
    - hexo-renderer-jade: 废弃，项目未使用
    - hexo-renderer-marked: 与 hexo-renderer-markdown-it 功能重叠
    
    解决方案: 移除上述废弃/冲突的渲染器，统一使用 hexo-renderer-markdown-it
    
    影响:
    - 减少 205 个包的安装
    - 消除渲染器冲突
    - 加快依赖安装速度
    
    * refactor: 移除未使用的 @mdit 插件和废弃渲染器
    
    移除的依赖:
    - @mdit/plugin-alert (未使用)
    - @mdit/plugin-figure (未使用)
    - @mdit/plugin-img-size (未使用)
    - hexo-browsersync (hexo 内置)
    - hexo-asset-img (与 hexo-asset 重复)
    - hexo-renderer-pug (未使用)
    - hexo-renderer-ejs (未使用)
    - hexo-diagrams-net (未使用)
    
    节省: 111 个包的安装
    
    同步修改:
    - scripts/filters/markdown_it.js 移除插件引用
    
    ---------
    
    Co-authored-by: h1s97x <3229724715002795-h1s97x@noreply.coze.cn>

--- scripts/filters/markdown_it.js
+++ scripts/filters/markdown_it.js
@@ -1,46 +1,8 @@
 'use strict';
 
-const { alert } = require('@mdit/plugin-alert');
-const { figure } = require('@mdit/plugin-figure');
-const { obsidianImgSize } = require('@mdit/plugin-img-size');
-// const { snippet } = require('@mdit/plugin-snippet');
+// @mdit/plugins 已移除 - 未在文章中使用
+// 旧插件: @mdit/plugin-alert, @mdit/plugin-figure, @mdit/plugin-img-size
 
 module.exports.customize = (parser) => {
-  parser.use(alert).use(figure).use(obsidianImgSize);
-
-  // parser.use(snippet, {
-  //   // `postPath` is provided by hexo-renderer-markdown-it
-  //   // See: https://github.com/hexojs/hexo-renderer-markdown-it/blob/master/lib/renderer.js
-  //   currentPath: (env) => env.postPath,
-  // });
+  // 预留扩展点，未来可按需添加插件
 };
-
-// 'use strict';
-
-// // 添加 console.log 来检查导出结构
-// const alertRaw = require('@mdit/plugin-alert');
-// const figureRaw = require('@mdit/plugin-figure');
-// const obsidianImageSizeRaw = require('@mdit/plugin-img-size');
-
-// console.log("@mdit/plugin-alert raw export type:", typeof alertRaw);
-// console.log("@mdit/plugin-alert raw export keys:", Object.keys(alertRaw));
-// console.log("@mdit/plugin-alert raw export:", alertRaw);
-
-// console.log("@mdit/plugin-figure raw export type:", typeof figureRaw);
-// console.log("@mdit/plugin-figure raw export keys:", Object.keys(figureRaw));
-// console.log("@mdit/plugin-figure raw export:", figureRaw);
-
-// console.log("@mdit/plugin-img-size raw export type:", typeof obsidianImageSizeRaw);
-// console.log("@mdit/plugin-img-size raw export keys:", Object.keys(obsidianImageSizeRaw));
-// console.log("@mdit/plugin-img-size raw export:", obsidianImageSizeRaw);
-
-// // 暂时注释掉实际的 use 调用，直到我们找到正确的加载方式
-// module.exports.customize = (parser) => {
-//   // parser.use(alertRaw.default || alertRaw); // 暂时注释
-//   // parser.use(figureRaw.default || figureRaw); // 暂时注释
-//   // parser.use(obsidianImageSizeRaw.default || obsidianImageSizeRaw); // 暂时注释
-
-//   // parser.use(snippet, {
-//   //   currentPath: (env) => env.postPath,
-//   // });
-// };

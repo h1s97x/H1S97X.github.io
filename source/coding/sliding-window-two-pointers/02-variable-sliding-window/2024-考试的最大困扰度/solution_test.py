ac28e93ec7e999b65f8f9332beb823e22dead696
h1s97x <yang1297656998@outlook.com>
2026-08-16 22:50:31 +0800

    feat(coding): 2024-考试的最大困扰度

---
+++ source/coding/sliding-window-two-pointers/02-variable-sliding-window/2024-考试的最大困扰度/solution_test.py
@@ -0,0 +1,14 @@
+import unittest
+from solution import Solution
+
+
+class Test2024(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+    
+    def test_example1(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
